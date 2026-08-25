bc651d7a3366579d887c93ab3fc212a8a989cbfb
Angel C. Martos <angelcmartos@gmail.com>
2026-08-25 17:01:17 +0200

    adding vite config

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/bubble-r3f',
+  base: '/',
   plugins: [react(), glsl(), tailwindcss()],
   resolve: {
     alias: {
