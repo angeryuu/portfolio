5ce3a81d8e676cc40ff4726d6561d88d6d240292
Angel C. Martos <angelcmartos@gmail.com>
2026-08-26 19:15:02 +0200

    centering experience

--- src/components/sections/experience/Experience.jsx
+++ src/components/sections/experience/Experience.jsx
@@ -15,10 +15,10 @@ export function Experience(){
                 {content.experience.items.map((item, index) => {
                     return (
                     <li key={index} className='experience-card' style={{ gridRow: index + 1 }}>
-                        <time className="block text-base font-boldmb-6">{item.date}</time>
-                        <h3 className="text-2xl text-primary font-bold mb-2">{item.role}</h3>
-                        <p className="text-base mb-4 font-bold">{item.company}</p>
-                        <p className="job-description text-justify text-sm">{item.description}</p>
+                        <time className="text-center md:text-justify block text-base font-boldmb-6">{item.date}</time>
+                        <h3 className="text-center md:text-justify text-2xl text-primary font-bold mb-2">{item.role}</h3>
+                        <p className="text-center md:text-justify text-base mb-4 font-bold">{item.company}</p>
+                        <p className="text-justify job-description text-sm">{item.description}</p>
                     </li>
                     )
                 })}
